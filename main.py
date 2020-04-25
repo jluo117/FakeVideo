4d82c124491998dfaa38108f5d1139856c5388fb
james luo <luojames52@gmail.com>
2020-04-25 15:51:53 -0700

    fix add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from VideoDetect import*
 videoTest = VideoDetect()
-print(videoTest.detect_video("cXBNJJ1adA4"))
+print(videoTest.detect_video("cVEemOmHw9Y"))
 print(videoTest.detect_video("u9GNf51_NvU"))
 print(videoTest.detect_video("7U-RbOKanYs"))
 print(videoTest.detect_video("WwfhLC16bis"))
