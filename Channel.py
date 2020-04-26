355e546301e4deeb2e05e28913b1bdb5ed094764
james luo <luojames52@gmail.com>
2020-04-26 01:49:08 -0700

    backend words

--- Channel.py
+++ Channel.py
@@ -1,28 +1,52 @@
 from bs4 import BeautifulSoup as bs
 import bisect
+import math  
 class Channel():
-	def __init__(self, soupData):
+	def __init__(self):
+		self.Channel_Url = None
+		self.Channel_Name = None
+		self.UpVote = 0
+		self.TotalVote = 0
+		self.Sub = ""
+		self.Freq = {}
+	 
+	def soup_insert(self, soupData):
 		channel_tag = soupData.find("div", attrs={"class": "yt-user-info"}).find("a")
 		#print(soupData.find("span", attrs={"class": "yt-subscriber-count"}))
 		self.Channel_Url = f"https://www.youtube.com{channel_tag['href']}"
 		self.Channel_Name = channel_tag.text
-		self.UpVote = 0
-		self.TotalVote = 0
+	
 		subData = soupData.find("span", attrs={"class": "yt-subscriber-count"})
 		if subData != None:
 			self.Sub = soupData.find("span", attrs={"class": "yt-subscriber-count"}).text.strip()
 		else:
-			self.Sub = "Error"
-		self.Freq = {}
+			self.Sub = "Not Found"
+		data = {
+		u'Channel_Url' : self.Channel_Url,
+		u'Channel_Name': self.Channel_Name,
+		u'Sub': self.Sub,
+		u'Freq': self.Freq,
+		u'UpVote': self.UpVote,
+		u'TotalVote': self.TotalVote
+		}
+		return data
+
+	def get_from_database(self,dataBase):
+		self.Channel_Url = dataBase['Channel_Url']
+		self.Channel_Name = dataBase['Channel_Name']
+		self.UpVote = dataBase['UpVote']
+		self.TotalVote = dataBase['TotalVote']
+		self.Sub = dataBase['Sub']
+		self.Freq = dataBase['Freq']
 	
 	def up_vote(self):
 		self.UpVote += 1
 		self.TotalVote += 1
-		
+		return self.UpVote , self.TotalVote
 	
 	def down_vote(self):
 		self.TotalVote += 1
-		
+		return self.TotalVote
 	def get_sub(self):
 		return self.Sub
 	def update_common(self,wordList):
@@ -31,6 +55,7 @@ class Channel():
 				self.Freq[word] += wordList[word]
 			else:
 				self.Freq[word] = wordList[word]
+		return self.Freq
 	def get_rating(self):
 
 		if self.TotalVote == 0:
@@ -39,7 +64,7 @@ class Channel():
 		print("UpVote")
 		print(self.UpVote)
 		print(self.TotalVote)
-		return (self.UpVote / self.TotalVote) * 5
+		return math.floor((self.UpVote / self.TotalVote) * 5)
 	def get_k_freqWords(self,K):
 		mostCommon = []
 		for word in self.Freq:
--- VideoDetect.py
+++ VideoDetect.py
@@ -4,135 +4,169 @@ from google.cloud.language import enums
 from google.cloud.language import types
 import reteriveChannel as rc
 import requests
+import os
 from Channel import Channel
 from bs4 import BeautifulSoup as bs
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import editdistance
 
-import editdistance 
-def correctData(Organzation,Proper):
-	massiveBucket = {}
-	for word in Organzation:
-		if word not in massiveBucket:
-			massiveBucket[word] = Organzation[word]
-		
-	for word in Proper:
-		if word not in massiveBucket:
-			massiveBucket[word] = Proper[word]
-		
-	for word in massiveBucket:
-		if massiveBucket[word] <= 1:
-			for toCompare in massiveBucket:
-				if editdistance.eval(word,toCompare) == 1:
-					massiveBucket[toCompare] += 1
-					massiveBucket[word] = 0
-					break
-	return massiveBucket
+
+def correctData(Organzation, Proper):
+    massiveBucket = {}
+    for word in Organzation:
+        if word not in massiveBucket:
+            massiveBucket[word] = Organzation[word]
+
+    for word in Proper:
+        if word not in massiveBucket:
+            massiveBucket[word] = Proper[word]
+
+    for word in massiveBucket:
+        if massiveBucket[word] <= 1:
+            for toCompare in massiveBucket:
+                if editdistance.eval(word, toCompare) == 1:
+                    massiveBucket[toCompare] += 1
+                    massiveBucket[word] = 0
+                    break
+    return massiveBucket
+
+
 def NLPProcessor(text):
-	OrganizationBucket = {}
-	ProperBucket = {}
-	client = language.LanguageServiceClient()
-	document = types.Document(
-    content=text,
-    type=enums.Document.Type.PLAIN_TEXT)
-	encoding_type = enums.EncodingType.UTF8
+    OrganizationBucket = {}
+    ProperBucket = {}
+    client = language.LanguageServiceClient()
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+    encoding_type = enums.EncodingType.UTF8
 
-	response = client.analyze_entities(document, encoding_type=encoding_type)
-	for entity in response.entities:
-		for mention in entity.mentions:
-			if enums.Entity.Type(entity.type).name == "ORGANIZATION":
-				if mention.text.content in OrganizationBucket:
-					OrganizationBucket[mention.text.content] += 1
-				else:
-					OrganizationBucket[mention.text.content] = 1
-			if enums.EntityMention.Type(mention.type).name == "PROPER":
-				if mention.text.content in ProperBucket:
-					ProperBucket[mention.text.content] += 1
-				else:
-					ProperBucket[mention.text.content] = 1
-	return OrganizationBucket , ProperBucket
+    response = client.analyze_entities(document, encoding_type=encoding_type)
+    for entity in response.entities:
+        for mention in entity.mentions:
+            if enums.Entity.Type(entity.type).name == "ORGANIZATION":
+                if mention.text.content in OrganizationBucket:
+                    OrganizationBucket[mention.text.content] += 1
+                else:
+                    OrganizationBucket[mention.text.content] = 1
+            if enums.EntityMention.Type(mention.type).name == "PROPER":
+                if mention.text.content in ProperBucket:
+                    ProperBucket[mention.text.content] += 1
+                else:
+                    ProperBucket[mention.text.content] = 1
+    return OrganizationBucket, ProperBucket
+
+
 class VideoDetect():
 
-	def __init__(self):
-		self.ChannelUrls = {}
-		self.DataSet = {}
-		self.lastChannel = None
-	def detect_video(self , url):
-		res = None
-		try:
-			res = YouTubeTranscriptApi.get_transcript(url)
-		except:
-			return "No Subtitles Found"
-		videoText = ""
-		time = 0
-		for output in res:
-			videoText += (output['text']) + " "
-			time += output['duration']
-		print(time/60)
-		OrgValue , ProperValue= NLPProcessor(videoText)
-		self.OrgValue = OrgValue
-		maxValue = (0,"")
-		massiveBucket = correctData(OrgValue,ProperValue)
-		for entitiies in OrgValue:
-			if massiveBucket[entitiies] == 0:
-				continue
-			maxValue = max(maxValue,(massiveBucket[entitiies],entitiies))
-		
-		
-		print(maxValue[0])
-		channelName , ChannelUrl,soup = rc.get_video_info(url)
-		if channelName == None:
-			self.lastChannel = "ERROR"
-			if maxValue[0] >= time/90:
-				
-				return "This video is an advertisement for " + maxValue[1]
-			else:
-				return "This video is not an advertisement"
-		self.lastChannel = channelName
-		if channelName not in self.DataSet:
-			newChannel = Channel(soup)
-			self.DataSet[channelName] = newChannel
-			self.ChannelUrls[ChannelUrl] = channelName
-		self.DataSet[channelName].update_common(massiveBucket)
-		rating = self.DataSet[channelName].get_rating()
-		if maxValue[0] >= time/90:
-			self.DataSet[channelName].down_vote()
-			print(self.DataSet[channelName].get_rating())
-			return "This video is an advertisement for " + maxValue[1]
-		self.DataSet[channelName].up_vote()
-		return "This video is not an advertisement"
+    def __init__(self):
+        
+        self.ChannelUrls = {}
+        self.DataSet = {}
+        self.lastChannel = None
+        cred = credentials.Certificate('steadfast-slate-275316-firebase-adminsdk-dnmsj-a25e6dd721.json')
+        firebase_admin.initialize_app(cred)
+        self.dataBase = firestore.client().collection('Channels')
+        #doc_ref = self.dataBase.collection('Channels').document('TechLead')
+        #doc = doc_ref.get()
+        #print(self.dataBase.to_dict())
+
+    def detect_video(self, url):
+        res = None
+        try:
+            res = YouTubeTranscriptApi.get_transcript(url)
+        except:
+            return "No Subtitles Found"
+        videoText = ""
+        time = 0
+        for output in res:
+            videoText += (output['text']) + " "
+            time += output['duration']
+        print(time/60)
+        OrgValue, ProperValue = NLPProcessor(videoText)
+        self.OrgValue = OrgValue
+        maxValue = (0, "")
+        massiveBucket = correctData(OrgValue, ProperValue)
+        for entitiies in OrgValue:
+            if massiveBucket[entitiies] == 0:
+                continue
+            maxValue = max(maxValue, (massiveBucket[entitiies], entitiies))
+
+        print(maxValue[0])
+        channelName, ChannelUrl, soup = rc.get_video_info(url)
+        if channelName == None:
+            self.lastChannel = "ERROR"
+            if maxValue[0] >= time/90:
+
+                return "This video is an advertisement for " + maxValue[1]
+            else:
+                return "This video is not an advertisement"
+        self.lastChannel = channelName
+        if channelName not in self.DataSet:
+            newChannel = Channel()
+            channelRef = self.dataBase.document(channelName).get()
 
 
-	def get_last_info(self):
-		if self.lastChannel == "ERROR" or self.lastChannel == None:
-			return "ERROR with either last channel or no data is found"
-		returnVal = "Channel Name: " + self.lastChannel + '\n'
-		returnVal += "Channel Sub: " + self.DataSet[self.lastChannel].get_sub() + ' \n'
-		returnVal += "Channel Rating: " + str(self.DataSet[self.lastChannel].get_rating()) + ' \n'
-		return (self.lastChannel, self.DataSet[self.lastChannel].get_sub(), str(self.DataSet[self.lastChannel].get_rating()))
+            if channelRef.exists:
+                newChannel.get_from_database(channelRef.to_dict())
+            else:
+                uploadData = newChannel.soup_insert(soup)
+                self.dataBase.document(channelName).set(uploadData)
+            self.DataSet[channelName] = newChannel
+            self.ChannelUrls[ChannelUrl] = channelName
+        Freq = self.DataSet[channelName].update_common(massiveBucket)
+        self.dataBase.document(channelName).update({
+            u'Freq' : Freq
+            })
+        rating = self.DataSet[channelName].get_rating()
+        if maxValue[0] >= time/90 - (5 * rating):
+            TotalVote = self.DataSet[channelName].down_vote()
+            self.dataBase.document(channelName).update({
+                u'TotalVote': TotalVote
+                })
 
+            #print(self.DataSet[channelName].get_rating())
+            return "This video is an advertisement for " + maxValue[1]
+        upvote , TotalVote = self.DataSet[channelName].up_vote()
+        self.dataBase.document(channelName).update({
+            u'UpVote': upvote,
+            u'TotalVote': TotalVote
+            })
+        return "This video is not an advertisement"
 
-	def get_channel_info(self, channel):
-		if channel not in self.DataSet:
-			return "Error"
-		returnVal = "Channel Name: " + channel + '\n'
-		returnVal += "Channel Sub: " + self.DataSet[channel].get_sub() + ' \n'
-		returnVal += "Channel Rating: " + str(self.DataSet[channel].get_rating()) + ' \n'
-		return returnVal
-	def get_popularVal(self,channel = None,K = 3):
-		if channel == None:
-			channel = self.lastChannel
-		if channel not in self.DataSet:
-			return "Invalid Channel"
-		KFreq = self.DataSet[channel].get_k_freqWords(K)
-		returnVal = {}
-		for word in KFreq:
-			returnVal[word[0]] = str(word[1])
-		return returnVal
-	def get_channel_name(self,url):
-		if url not in self.ChannelUrls:
-			return None
-		else:
-			return self.ChannelUrls[url]
-		
+    def get_last_info(self):
+        if self.lastChannel == "ERROR" or self.lastChannel == None:
+            return "ERROR with either last channel or no data is found"
+        returnVal = "Channel Name: " + self.lastChannel + '\n'
+        returnVal += "Channel Sub: " + \
+            self.DataSet[self.lastChannel].get_sub() + ' \n'
+        returnVal += "Channel Rating: " + \
+            str(self.DataSet[self.lastChannel].get_rating()) + ' \n'
+        return (self.lastChannel, self.DataSet[self.lastChannel].get_sub(), str(self.DataSet[self.lastChannel].get_rating()))
 
+    def get_channel_info(self, channel):
+        if channel not in self.DataSet:
+            return "Error"
+        returnVal = "Channel Name: " + channel + '\n'
+        returnVal += "Channel Sub: " + self.DataSet[channel].get_sub() + ' \n'
+        returnVal += "Channel Rating: " + \
+            str(self.DataSet[channel].get_rating()) + ' \n'
+        return returnVal
 
+    def get_popularVal(self, channel=None, K=3):
+        if channel == None:
+            channel = self.lastChannel
+        if channel not in self.DataSet:
+            return "Invalid Channel"
+        KFreq = self.DataSet[channel].get_k_freqWords(K)
+        returnVal = {}
+        for word in KFreq:
+            returnVal[word[0]] = str(word[1])
+        return returnVal
 
+    def get_channel_name(self, url):
+        if url not in self.ChannelUrls:
+            return None
+        else:
+            return self.ChannelUrls[url]
